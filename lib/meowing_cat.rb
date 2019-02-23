6f93e471aae700d20ddb73e939cb187c33c51ae3
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-23 21:12:49 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
-## code your solution here. 
+class Cat
+  attr_accessor :name,:meow
+  
+  def initialize()
+    @name = name
+  end 
+  def meow()
+    puts "meow!"
+
+end 
+end
+maru = Cat.new()
+
+
